85d99c7bc5dcac918b9718bc29197fc2b95c9ea6
jefferson <jefferson.science@gmail.com>
2023-09-30 21:52:27 -0300

    0.1.0
    Todo:
    |- Melhorar o timestamp para renomear arquivos.
    `- Manipular erros de permissão.

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use std::{
+    env, 
+    fs::{rename/*, File, Write*/, create_dir_all}, 
+    path::{Path, PathBuf},
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+fn main() {
+    let files: Vec<String> = env::args().collect();
+    if files.len() < 2 {
+        eprintln!("Uso: {} <file1> <file2> ...", files[0]);
+        std::process::exit(1);
+    }
+
+    let dest_dir = match env::var("LIXEIRA") {
+        Ok(val) => {
+            val
+        },
+        Err(_) => {
+            eprintln!("Erro: A variável $LIXEIRA não está definida.");
+            std::process::exit(1);
+        }
+    };
+
+    if !Path::new(&dest_dir).exists() {
+        if let Err(err) = create_dir_all(&dest_dir) {
+            eprintln!("Erro ao criar a lixeira em \"{}\"", err);
+            std::process::exit(1);
+        }
+    }
+
+    for file in &files[1..] {
+        let source_file = Path::new(&file);
+        let mut dest_file = PathBuf::from(&dest_dir);
+        let source_filename = Path::new(&file)
+            .file_name()
+            .expect("Invalid source file path");
+        dest_file.push(&source_filename);
+
+        while dest_file.exists() {
+            let timestamp = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_secs();
+            let timestamp_str = format!("{}", timestamp);
+            let mut new_filename = source_filename.to_os_string();
+            new_filename.push("_");
+            new_filename.push(&timestamp_str);
+            dest_file = PathBuf::from(&dest_dir);
+            dest_file.push(&new_filename);
+        }
+
+        match rename(&source_file, &dest_file) {
+            Ok(_) => println!("Moved {} to {}", 
+                            source_file.to_string_lossy(), 
+                            dest_file.to_string_lossy()),
+            Err(err) => eprintln!("Error moving {}: {}", 
+                            source_file.to_string_lossy(), 
+                            err),
+        }
+    }
+}
